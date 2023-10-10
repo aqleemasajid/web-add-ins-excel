9a27a03bcb37dceca2e89e5395074dd7415f05e5
aqleemasajid <67157834+aqleemasajid@users.noreply.github.com>
2023-10-10 19:14:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,53 +13,23 @@ function updateStatus(message) {
     var statusInfo = $('#status');
     statusInfo[0].innerHTML += message + "<br/>";
 }
+
 async function sendFile() {
-    await Excel.run(async (context) => {
-        var worksheets = context.workbook.worksheets;
-        worksheets.load("items/name");
+    Excel.run(function (context) {
+        var sheet = context.workbook.worksheets.getActiveWorksheet();
+        var range = sheet.getUsedRange();
+        range.load("values"); // Load the values within the range
         
-        await context.sync();
-
-        if (worksheets.items.length > 0) {
-            var activeWorksheet = context.workbook.worksheets.getActiveWorksheet();
-            activeWorksheet.getUsedRange().load("values"); // Load the used range values
-
-            await context.sync();
-
-            var data = activeWorksheet.values; // Get the data from the used range
-
-            // Convert the data to a CSV format
-            var csvData = data.map(row => row.join(',')).join('\n');
-
-            // Create a Blob with the CSV content
-            var blob = new Blob([csvData], { type: 'text/csv' });
-
-            // Create an Object URL for the Blob
-            var url = URL.createObjectURL(blob);
-
-            // Create a download link
-            var a = document.createElement('a');
-            a.href = url;
-            a.download = 'webAddIn_' + generateGuid() + '.csv'; // Specify the desired file name
-            a.style.display = 'none';
-
-            // Append the link to the document body and trigger the download
-            document.body.appendChild(a);
-            a.click();
-
-            // Clean up by revoking the Object URL
-            URL.revokeObjectURL(url);
-        } else {
-            updateStatus('No worksheets found in the workbook.');
-        }
+        return context.sync()
+            .then(function () {
+                // Access the data
+                var data = range.values;
+                // Handle the data (e.g., convert it to a downloadable format)
+            });
+    }).catch(function (error) {
+        console.log(error);
     });
+    
 }
 
-function generateGuid() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0,
-            v = c === 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
 
