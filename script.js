3f9c0dac85ff4879336ae17aee7ddf78797caac8
aqleemasajid <67157834+aqleemasajid@users.noreply.github.com>
2023-10-10 15:40:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,38 +13,53 @@ function updateStatus(message) {
     var statusInfo = $('#status');
     statusInfo[0].innerHTML += message + "<br/>";
 }
-
 async function sendFile() {
     await Excel.run(async (context) => {
-        let sheets = context.workbook.worksheets;
-        sheets.load("items/name");
-
+        var worksheets = context.workbook.worksheets;
+        worksheets.load("items/name");
+        
         await context.sync();
 
-        if (sheets.items.length > 1) {
-            updateStatus(`There are ${sheets.items.length} worksheets in the workbook:`);
+        if (worksheets.items.length > 0) {
+            var activeWorksheet = context.workbook.worksheets.getActiveWorksheet();
+            activeWorksheet.getUsedRange().load("values"); // Load the used range values
+
+            await context.sync();
+
+            var data = activeWorksheet.values; // Get the data from the used range
+
+            // Convert the data to a CSV format
+            var csvData = data.map(row => row.join(',')).join('\n');
+
+            // Create a Blob with the CSV content
+            var blob = new Blob([csvData], { type: 'text/csv' });
+
+            // Create an Object URL for the Blob
+            var url = URL.createObjectURL(blob);
+
+            // Create a download link
+            var a = document.createElement('a');
+            a.href = url;
+            a.download = 'webAddIn_' + generateGuid() + '.csv'; // Specify the desired file name
+            a.style.display = 'none';
+
+            // Append the link to the document body and trigger the download
+            document.body.appendChild(a);
+            a.click();
+
+            // Clean up by revoking the Object URL
+            URL.revokeObjectURL(url);
         } else {
-            updateStatus(`There is one worksheet in the workbook:`);
+            updateStatus('No worksheets found in the workbook.');
         }
-
-        sheets.items.forEach(function (sheet) {
-            updateStatus(JSON.stringify(sheet));
-
-            var request = new XMLHttpRequest();
-
-
-            request.open("POST", "https://reqres.in/api/users");
-            //request.setRequestHeader("Slice-Number", slice.index);
-
-            request.send(JSON.stringify(sheet));
-
-        });
-
-
-
-
     });
 }
 
-
+function generateGuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
 
